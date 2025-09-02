82042bd10018565cca0c9363295d2dab4bb4708a
otherbcrypto <support@otherb.com>
2025-09-02 18:21:16 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -123,7 +123,7 @@ public:
         assert(genesis.hashMerkleRoot == uint256S("0x8ebc5bef4db94f56c0e983444f7313f86e32204f4a9cae5706f82a0b5e162b19"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
-        vSeeds.emplace_back("node.otherb.com", true);
+        vSeeds.emplace_back("seed.otherb.com", true);
         vSeeds.emplace_back("relay.otherb.com", true);
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,25);
@@ -209,7 +209,7 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
-        vSeeds.emplace_back("node.otherb.com", true);
+        vSeeds.emplace_back("seed.otherb.com", true);
         vSeeds.emplace_back("relay.otherb.com", true);
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
